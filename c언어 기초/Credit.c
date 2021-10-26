659cc9abfd307108399de996df586cd8648ddcff
이루이루 <61938768+wnstj7788@users.noreply.github.com>
2021-10-26 10:07:56 +0000

    연산자

---
+++ c언어 기초/Credit.c
@@ -0,0 +1,25 @@
+/*
+#include<stdio.h>
+
+int main(void)
+{
+  int score = 85; // 학생의 점수
+  if(score >= 90){
+    printf("당신의 학점은 A입니다");
+  }
+  else if(score >=80){   
+    printf("당신의 학점은 B입니다");
+
+  }
+  else if(score >=70){   
+    printf("당신의 학점은 C입니다");
+
+  }
+  else{
+    printf("당신의 학점은 F입니다");
+
+  }
+
+  return 0;
+}
+*/
